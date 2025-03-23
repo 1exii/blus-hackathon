e5eb02cd57da7d6ee69eb7a83e6baac5b2c26f0f
Lexi Li <67660291+starxskiez@users.noreply.github.com>
2025-03-23 09:13:56 -0700

    Fix Get Started Link Buttons

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,8 +1,6 @@
 import styles from "../style";
 import Button from "./Button";
 
-
-/*Call to Action*/
 const CTA = () => (
   <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
     <div className="flex-1 flex flex-col">
@@ -13,7 +11,7 @@ const CTA = () => (
     </div>
 
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-      <Button />
+      <Button link="/product"/>
     </div>
   </section>
 );
